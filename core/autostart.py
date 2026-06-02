e4b49336e90400923d393a09962d2626f9fed2cd
Borisov-Evgeniy <evgeniy.borisov@yahoo.com>
2026-06-02 12:16:57 +0300

    Add autostart.py

---
+++ core/autostart.py
@@ -0,0 +1,63 @@
+import sys
+import winreg
+
+from core.config import APP_NAME
+
+
+class AutoStart():
+    REG_PATH = (
+        r"Software\Microsoft\Windows"
+        r"\CurrentVersion\Run"
+    )
+
+    @classmethod
+    def enable(cls) -> None:
+        if not getattr(sys, "frozen", False):
+            return
+
+        exe_path = sys.executable
+
+        with winreg.OpenKey(
+                winreg.HKEY_LOCAL_MACHINE,
+                cls.REG_PATH,
+                0,
+                winreg.KEY_ALL_ACCESS,
+        ) as key:
+            winreg.SetValueEx(
+                    key,
+                    APP_NAME,
+                    0,
+                    winreg.REG_SZ,
+                    exe_path,)
+
+    @classmethod
+    def disable(cls) -> None:
+        try:
+            with winreg.OpenKey(
+                    winreg.HKEY_CURRENT_USER,
+                    cls.REG_PATH,
+                    0,
+                    winreg.KEY_SET_VALUE,
+            ) as key:
+                winreg.DeleteValue(
+                    key,
+                    APP_NAME,
+                )
+
+        except FileNotFoundError:
+            pass
+
+    @classmethod
+    def is_enabled(cls) -> bool:
+        try:
+            with winreg.OpenKey(
+                winreg.HKEY_CURRENT_USER,
+                cls.REG_PATH,
+            ) as key:
+                winreg.SetValueEx(
+                    key,
+                    APP_NAME,
+                )
+            return True
+        except FileNotFoundError:
+            return False
