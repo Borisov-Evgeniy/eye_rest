24ddba6e77e54d77bebf4488455dd475d346ad8b
Borisov-Evgeniy <evgeniy.borisov@yahoo.com>
2026-06-03 16:59:02 +0300

    add lock break window

--- ui/break_window.py
+++ ui/break_window.py
@@ -5,6 +5,7 @@ class BreakWindow(tk.Toplevel):
 
     def __init__(self, parent, break_minutes, on_finish):
         super().__init__(parent)
+        self.resizable(False, False)
         self.seconds = break_minutes * 60
         self.on_finish = on_finish
         self.title("Перерыв")
@@ -23,6 +24,10 @@ class BreakWindow(tk.Toplevel):
         self.geometry(f"{width}x{height}+{x}+{y}")
         self.attributes("-toolwindow", True)
         self.attributes("-topmost", True)
+        self._fixed_x = x
+        self._fixed_y = y
+
+        self.after(100, self._lock_position)
 
         tk.Label(self,text="Сделай перерыв для глаз!",font=("Arial", 18),).pack(pady=20)
         self.timer_label = tk.Label(self,font=("Arial", 24),)
@@ -43,6 +48,13 @@ class BreakWindow(tk.Toplevel):
             self.after(1000, self.update_timer)
         else:
             self.force_close()
+
+    def _lock_position(self):
+
+        self.geometry(f"{self.winfo_width()}x{self.winfo_height()}"
+                      f"+{self._fixed_x}+{self._fixed_y}")
+
+        self.after(100, self._lock_position)
 
     def disable_close(self):
         pass
