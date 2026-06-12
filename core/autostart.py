f71b85384a357e0e4caff51dc0a6766587c2eed7
Borisov-Evgeniy <evgeniy.borisov@yahoo.com>
2026-06-12 17:15:02 +0300

    update autostart and added linux compatibility

--- core/autostart.py
+++ core/autostart.py
@@ -1,59 +1,103 @@
 import sys
+from pathlib import Path
 import winreg
-
 from core.config import APP_NAME
 
 
 class AutoStart():
-    REG_PATH = (
-        r"Software\Microsoft\Windows"
-        r"\CurrentVersion\Run"
-    )
+    if sys.platform == "win32":
+        REG_PATH = (r"Software\Microsoft\Windows"
+                    r"\CurrentVersion\Run")
 
     @classmethod
     def enable(cls) -> None:
+        if sys.platform == "win32":
+            cls._enable_windows()
+        else:
+            cls._enable_linux()
+
+    @classmethod
+    def disable(cls):
+        if sys.platform == "win32":
+            cls._disable_windows()
+        else:
+            cls._disable_linux()
+
+    def is_enabled(cls):
+        if sys.platform == "win32":
+            return cls._is_enabled_winodws()
+        else:
+            return cls._is_enabled_linux()
+
+# ============= Windows ========================== #
+    @classmethod
+    def _enable_windows(cls) -> None:
         if not getattr(sys, "frozen", False):
             return
 
         exe_path = sys.executable
 
-        with winreg.OpenKey(
-                winreg.HKEY_CURRENT_USER,
+        with cls.winreg.OpenKey(
+                cls.winreg.HKEY_CURRENT_USER,
                 cls.REG_PATH,
                 0,
-                winreg.KEY_SET_VALUE,
-        ) as key:
-            winreg.SetValueEx(
-                    key,
-                    APP_NAME,
-                    0,
-                    winreg.REG_SZ,
-                    exe_path,)
+                cls.winreg.KEY_SET_VALUE,
+        ) as key: cls.winreg.SetValueEx(key, APP_NAME, 0, cls.winreg.REG_SZ, exe_path)
 
     @classmethod
-    def disable(cls) -> None:
+    def _disable_windows(cls) -> None:
         try:
-            with winreg.OpenKey(
-                    winreg.HKEY_CURRENT_USER,
+            with cls.winreg.OpenKey(
+                    cls.winreg.HKEY_CURRENT_USER,
                     cls.REG_PATH,
                     0,
-                    winreg.KEY_SET_VALUE,
-            ) as key:
-                winreg.DeleteValue(
-                    key,
-                    APP_NAME,
-                )
+                    cls.winreg.KEY_SET_VALUE,
+            ) as key: cls.winreg.DeleteValue(key,APP_NAME,)
 
         except FileNotFoundError:
             pass
 
     @classmethod
-    def is_enabled(cls) -> bool:
+    def is_enabled_windows(cls) -> bool:
         try:
-            with winreg.OpenKey(
-                winreg.HKEY_CURRENT_USER,
+            with cls.winreg.OpenKey(
+                cls.winreg.HKEY_CURRENT_USER,
                 cls.REG_PATH,) as key:
-                winreg.QueryValueEx(key,APP_NAME,)
+                cls.winreg.QueryValueEx(key,APP_NAME,)
             return True
         except FileNotFoundError:
             return False
+
+# === === === === === Linux === === === === === #
+    @classmethod
+    def _enable_linux(cls) -> None:
+        if not getattr(sys, "frozen", False):
+            return
+
+        exe_path = sys.executable
+
+        autostart_dir = Path.home() /".congig" / "autostart"
+        autostart_dir.mkdir(parents=True, exist_ok=True)
+
+        desktop_file = autostart_dir / "eye-rest.desktop"
+
+        content = f"""[Desktop Entry]
+        Type=Application
+        Name={APP_NAME}
+        Exec={exe_path}
+        Hidden=false
+        NoDisplay=false
+        X-GNOME-Autostart-enabled=true
+        Comment=Eye Rest Application
+        """
+        desktop_file.write_text(content)
+
+    @classmethod
+    def _disable_linux(cls) -> None:
+        desktop_file = Path.home() / ".config" / "autostart" / "eye-rest.desktop"
+        if desktop_file.exists():
+            desktop_file.unlink()
+
+    def is_enabled_linux(cls) -> bool:
+        desktop_file = Path.home() / ".config" / "autostart" / "eye-rest.desktop"
+        return desktop_file.exists()
--- core/config.py
+++ core/config.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-APP_NAME = "Eye Rest"
+APP_NAME = "EyeRest"
 BASE_DIR = Path(__file__).resolve().parent.parent
 SETTINGS_FILE = BASE_DIR / "settings.json"
