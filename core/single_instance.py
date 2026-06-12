39483a3eec1ae04d3c8155ec15278a92370d5321
Borisov-Evgeniy <evgeniy.borisov@yahoo.com>
2026-06-12 16:00:41 +0300

    update single_instance\ added linux compatibility

--- core/single_instance.py
+++ core/single_instance.py
@@ -1,36 +1,75 @@
+import sys
+import os
+import fcntl
 import ctypes
 from ctypes import wintypes
+from pathlib import Path
+
 
 class SingleInstance:
-    def __init__(self, name: str = "EyeRest_SingleInstance"):
+    def __init__(self, name: str = "EyeRest"):
         self.name = name
-        self.mutex = None
+
+        self.lock_file = None
+        self.lock_fd = None
+
+    def acquire(self) -> bool:
+        if sys.platform == "win32":
+            return self._acquire_windows()
+        else:
+            return self._acquire_unix()
+
+    def _acquire_windows(self):
         self._kernel32 = ctypes.windll.kernel32
         self._kernel32.CreateMutexW.restype = wintypes.HANDLE
-        self._kernel32.CreateMutexW.argtypes = [wintypes.LPVOID,wintypes.BOOL,wintypes.LPCWSTR]
+        self._kernel32.CreateMutexW.argtypes = [wintypes.LPVOID, wintypes.BOOL, wintypes.LPCWSTR]
+
         self._kernel32.GetLastError.restype = wintypes.DWORD
+
         self._kernel32.CloseHandle.restype = wintypes.BOOL
         self._kernel32.CloseHandle.argtypes = [wintypes.HANDLE]
 
-    def acquire(self) -> bool:
-        if self.mutex is not None:
-            return True  # уже захвачен
+        self.lock_file = self._kernel32.CreateMutexW(None, False, self.name)
 
-        self.mutex = self._kernel32.CreateMutexW(None, False, self.name)
-
-        if not self.mutex:
+        if not self.lock_file:
             return False
 
         last_error = self._kernel32.GetLastError()
 
-        if last_error == 183:  # ERROR_ALREADY_EXISTS
-            # Уже запущен другой экземпляр
-            self._kernel32.CloseHandle(self.mutex)
-            self.mutex = None
+        if last_error == 183:
+            self._kernel32.CloseHandle(self.lock_file)
+            self.lock_file = None
             return False
         return True
 
+    def _acquire_unix(self):
+        lock_dir = Path.home() / ".cache" / "eye_rest"
+        lock_dir.mkdir(parents=True, exist_ok=True)
+        self.lock_file = lock_dir / "lock"
+
+        try:
+            self.lock_fd = open(self.lock_file, 'w')
+            fcntl.flock(self.lock_fd.fileno(), fcntl.LOCK_EX | fcntl.LOCK_NB)
+            self.lock_fd.write(str(os.getpid()))
+            self.lock_fd.flush()
+            return True
+        except(IOError,OSError):
+            if self.lock_fd:
+                self.lock_fd.close()
+            return False
+
     def release(self):
-        if self.mutex:
-            self._kernel32.CloseHandle(self.mutex)
-            self.mutex = None
+        if sys.platform == "win32":
+            if self.lock_file:
+                self._kernel32.CloseHandle(self.lock_file)
+                self.lock_file = None
+            else:
+                if self.lock_fd:
+                    try:
+                        fcntl.flock(self.lock_fd.fileno(), fcntl.LOCK_UN)
+                        self.lock_fd.close()
+
+                        if self.lock_fd and self.lock_file.exists():
+                            self.lock_file.unlink()
+                    except:
+                        pass
